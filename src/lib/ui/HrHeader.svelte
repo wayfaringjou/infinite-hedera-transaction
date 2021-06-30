b969c8037df10522bce69eb2bca6189c3891f21d
Joel Del Cueto <wayfaringjou@gmail.com>
2021-06-29 22:37:05 -0400

    Fix header style and add more funds to demo accounts

--- src/lib/ui/HrHeader.svelte
+++ src/lib/ui/HrHeader.svelte
@@ -3,16 +3,4 @@
 </script>
 
 <h3>{header}</h3>
-<div class="header-hr" />
-
-<style>
-	.header-hr {
-		@apply border-solid;
-		@apply border-b-2;
-		@apply border-gray-400;
-		@apply w-full;
-		@apply -mt-2px;
-		@apply relative;
-		@apply -z-1;
-	}
-</style>
+<div class="header-hr border-solid border-b-2 border-gray-400 w-full -mt-2px relative -z-1" />
--- src/lib/utils/hederaOperations.ts
+++ src/lib/utils/hederaOperations.ts
@@ -118,7 +118,7 @@ const createOperator = (account: string, privateKey: string | null = null) => ({
 			const newAccountData = await this.retryOnStreamErr(3, async () => {
 				const transaction = new AccountCreateTransaction()
 					.setKey(newAccountPublicKey)
-					.setInitialBalance(new Hbar(10));
+					.setInitialBalance(new Hbar(100));
 
 				const txResponse = await transaction.execute(this.client);
 
